7bad9737be15c920b9e9bd3d1bb2d94fe280b4e2
sovaleow <leowweiru@gmail.com>
2026-08-24 14:44:49 +0800

    Initial project structure

---
+++ as-2/scripts/apple.gd
@@ -0,0 +1,18 @@
+extends Area2D
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@onready var collected_sound: AudioStreamPlayer2D = $CollectedSound
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_body_entered(body: Node2D) -> void:
+	animated_sprite_2d.animation = "collected"
+	collected_sound.play()
